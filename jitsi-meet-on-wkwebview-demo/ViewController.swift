43b9d449b00cf5d4b5199aa1526af8781f35efce
Huong Nguyen <huong.nguyen@ubitec.ch>
2020-12-14 11:10:20 +0700

    Initial Commit

---
+++ jitsi-meet-on-wkwebview-demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  jitsi-meet-on-wkwebview-demo
+//
+//  Created by Van Huong on 12/14/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
