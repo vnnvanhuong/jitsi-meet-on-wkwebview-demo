9cb67f67e4fcaf839e7b99f32bd1c0fddf6bcc3a
Huong Nguyen <huong.nguyen@ubitec.ch>
2020-12-23 10:05:58 +0700

    remove Pods folder

--- Pods/Target Support Files/Pods-jitsi-meet-on-wkwebview-demo/Pods-jitsi-meet-on-wkwebview-demo-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_jitsi_meet_on_wkwebview_demo : NSObject
-@end
-@implementation PodsDummy_Pods_jitsi_meet_on_wkwebview_demo
-@end
--- Pods/Target Support Files/WKWebViewRTC/WKWebViewRTC-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_WKWebViewRTC : NSObject
-@end
-@implementation PodsDummy_WKWebViewRTC
-@end
